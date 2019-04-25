03c16ba8ccf8f61c58f3502c8d9b27b1a4d0b29b
AdnaB <45564053+AdnaB@users.noreply.github.com>
2019-04-25 18:05:51 +0200

    Update filesToList.py

--- filesToList.py
+++ filesToList.py
@@ -1,4 +1,6 @@
 from os import listdir
 from os.path import isfile, join
 mypath = 'C:/Users/adnab/deepLearningTest/best-artworks-of-all-time/resized/resized'
-onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+artists = [f.split('_')[0] for f in allfiles]
+
