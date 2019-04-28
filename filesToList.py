7037dbdabad75028cd84d8cc0adc5d18906474e0
AdnaB <45564053+AdnaB@users.noreply.github.com>
2019-04-28 19:50:29 +0200

    Added list of labels as numbers

--- filesToList.py
+++ filesToList.py
@@ -1,6 +1,16 @@
 from os import listdir
 from os.path import isfile, join
 mypath = 'C:/Users/adnab/deepLearningTest/best-artworks-of-all-time/resized/resized'
-allfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-artists = [f.split('_')[0] for f in allfiles]
+onlyfiles = [f.split('_')[0] for f in listdir(mypath) if isfile(join(mypath, f))]
 
+
+labels = []
+label  = 0
+currentArtist = onlyfiles[0]
+for f in onlyfiles:
+	if currentArtist != f:
+		label = label +1
+		currentArtist = f
+	labels.append(label)
+
+	
