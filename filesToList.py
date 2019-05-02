cb73157c547ffa18b83e49b87874995ba780a0a3
Sanne <sanneeggengoor@gmail.com>
2019-05-02 16:25:10 +0200

    moee middag

--- filesToList.py
+++ filesToList.py
@@ -1,16 +1,16 @@
 from os import listdir
 from os.path import isfile, join
-mypath = 'C:/Users/adnab/deepLearningTest/best-artworks-of-all-time/resized/resized'
+mypath = './resized/resizebyme'
 onlyfiles = [f.split('_')[0] for f in listdir(mypath) if isfile(join(mypath, f))]
 
+def ftl():
 
-labels = []
-label  = 0
-currentArtist = onlyfiles[0]
-for f in onlyfiles:
-	if currentArtist != f:
-		label = label +1
-		currentArtist = f
-	labels.append(label)
-
-	
+	labels = []
+	label  = 0
+	currentArtist = onlyfiles[0]
+	for f in onlyfiles:
+		if currentArtist != f:
+			label = label +1
+			currentArtist = f
+		labels.append(label)
+	return labels
--- resizer.py
+++ resizer.py
@@ -3,16 +3,62 @@ from PIL import Image
 import cv2 as cv
 import filesToList as ftl
 import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import csv
 
 
 
-# i = 0
-# for filename in os.listdir("./resized/resized"):
-#     im = cv.imread("./resiszed/resized/"+filename)
-#     resim = cv.resize(im,(224,224))
-#     cv.imwrite("./resized/resizebyme/"+filename, resim)
-#     print(i)
-#     i+=1
+def resize():
+    i = 0
+    for filename in os.listdir("./resized/resized"):
+        im = cv.imread("./resized/resized/"+filename)
+        resim = cv.resize(im,(224,224))
+        cv.imwrite("./resized/resizebyme/"+filename, resim)
+        print(i)
+        i+=1
+
+def split():
+    artistlist = []
+    with open('artists.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                list = row[1].split()
+                artistlist += [list[0]]
+
+    # print(artistlist)
+    filelist = os.listdir("./resized/resizebyme")
+    labellist = []
+    for filename in filelist:
+        name = filename.split('_')[0]
+        labellist += [artistlist.index(name)]
+    # print(labellist)
+    d = {"names": filelist, "labels" : labellist}
+    df = pd.DataFrame(data=d)
+    X_train, X_testt, y_train, y_testt = train_test_split(filelist,labellist, test_size = 0.2)
+    X_test, X_val, y_test, y_val = train_test_split(X_testt,y_testt, train_size = 0.5)
+
+    with open('train.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        for i in range(len(X_train)):
+            writer.writerow([X_train[i], y_train[i]])
+    csvFile.close()
+
+    with open('test.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        for i in range(len(X_test)):
+            writer.writerow([X_test[i], y_test[i]])
+    csvFile.close()
+
+    with open('val.csv', 'w') as csvFile:
+        writer = csv.writer(csvFile)
+        for i in range(len(X_val)):
+            writer.writerow([X_val[i], y_val[i]])
+    csvFile.close()
 
 def getBatch(filenamelist):
 
@@ -28,3 +74,5 @@ def getBatch(filenamelist):
         # labelArray = ftl.ftl()
         im.close()
     return dataArray
+
+split()
